ed4b8a408351cce1733ca0f1192c76e4223912cb
Sean Sutherland <seansutherland24@gmail.com>
2021-01-26 11:09:44 -0500

    Add fragments

---
+++ app/src/main/java/com/example/whiskeybuddy/AddWhiskeyActivity.java
@@ -0,0 +1,13 @@
+package com.example.whiskeybuddy;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddWhiskeyActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
